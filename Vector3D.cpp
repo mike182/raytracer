a012418eee6ccce87d081fb0c4872e09623b5731
mike <rodrig.tek@gmail.com>
2022-01-25 04:03:06 +0100

    operators & maths

--- Vector3D.cpp
+++ Vector3D.cpp
@@ -1,5 +1,10 @@
 #include "Vector3D.hpp"
 
+#include "Normal.hpp"
+#include "Point3D.hpp"
+
+#include <cmath>
+
 Vector3D::Vector3D(void)
     :  x(0), y(0), z(0) { }
 
@@ -12,6 +17,12 @@ Vector3D::Vector3D(double x, double y, double z)
 Vector3D::Vector3D(const Vector3D & v)
     : x(v.x), y(v.y), z(v.z) { }
 
+Vector3D::Vector3D(const Normal & n)
+    : x(n.x), y(n.y), z(n.z) { }
+
+Vector3D::Vector3D(const Point3D & p)
+    : x(p.x), y(p.y), z(p.z) { }
+
 Vector3D::~Vector3D(void)
 {
 }
@@ -20,9 +31,90 @@ Vector3D & Vector3D::operator=(const Vector3D & rhs) {
     if (this == &rhs)
         return *this;
 
-    // x = rhs.x;
-    // y = rhs.y;
-    // z = rhs.z;
+    x = rhs.x;
+    y = rhs.y;
+    z = rhs.z;
+
+    return *this;
+}
+
+Vector3D & Vector3D::operator=(const Normal & rhs) {
+    x = rhs.x;
+    y = rhs.y;
+    z = rhs.z;
+
+    return *this;
+}
+
+Vector3D & Vector3D::operator=(const Point3D & rhs) {
+    x = rhs.x;
+    y = rhs.y;
+    z = rhs.z;
+
+    return *this;
+}
+
+Vector3D Vector3D::operator-() const {
+    return Vector3D(-x, -y, -z);
+}
+
+Vector3D Vector3D::operator*(const double a) const {
+    return Vector3D(x * a, y * a, z * a);
+}
+
+Vector3D operator*(const double a, const Vector3D & v) {
+    return Vector3D(a * v.x, a * v.y, a * v.z);
+}
+
+Vector3D Vector3D::operator/(const double a) const {
+    return Vector3D(x / a, y / a, z / a);
+}
+
+Vector3D Vector3D::operator+(const Vector3D & v) const {
+    return Vector3D(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3D & Vector3D::operator+=(const Vector3D & v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+}
+
+Vector3D Vector3D::operator-(const Vector3D & v) const {
+    return Vector3D(x - v.x, y - v.y, z - v.z);
+}
+
+double Vector3D::operator*(const Vector3D & v) const {
+    return x * v.x + y * v.y + z * v.z;
+}
+
+Vector3D Vector3D::operator^(const Vector3D & v) const {
+    return Vector3D(y * v.z - z * v.y,
+                    z * v.x - x * v.z,
+                    x * v.y - y * v.x);
+}
+
+double Vector3D::length() {
+    return sqrt(x * x + y * y + z * z);
+}
+
+double Vector3D::len_squared() {
+    return x * x + y * y + z * z;
+}
+
+void Vector3D::normalize() {
+    double length = sqrt(x * x + y * y + z * z);
+    x /= length;
+    y /= length;
+    z /= length;
+}
+
+Vector3D & Vector3D::hat() {
+    double length = sqrt(x * x + y * y + z * z);
+    x /= length;
+    y /= length;
+    z /= length;
 
     return *this;
 }
--- Vector3D.hpp
+++ Vector3D.hpp
@@ -1,18 +1,41 @@
 #ifndef __VECTOR3D_HPP__
 #define __VECTOR3D_HPP__
 
+class Normal;
+class Point3D;
+
 class Vector3D {
 public:
     Vector3D(void);
     Vector3D(double a);
     Vector3D(double x, double y, double z);
     Vector3D(const Vector3D & v);
+    Vector3D(const Normal & n);
+    Vector3D(const Point3D & p);
     virtual ~Vector3D(void);
 
     Vector3D & operator=(const Vector3D & rhs);
+    Vector3D & operator=(const Normal & rhs);
+    Vector3D & operator=(const Point3D & rhs);
+
+    Vector3D operator-() const;
+    Vector3D operator*(const double a) const;
+    Vector3D operator/(const double a) const;
+    Vector3D operator+(const Vector3D & v) const;
+    Vector3D & operator+=(const Vector3D & v);
+    Vector3D operator-(const Vector3D & v) const;
+    double operator*(const Vector3D & v) const;
+    Vector3D operator^(const Vector3D & v) const;
+
+    double length();
+    double len_squared();
+    void normalize();
+    Vector3D & hat();
 
 public:
     double x, y, z;
 };
+
+Vector3D operator*(const double a, const Vector3D & v); // prototype
 
 #endif // __VECTOR3D_HPP__
