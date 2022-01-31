fa7230de7079fef09294f9a5292487257349f8b7
mike <rodrig.tek@gmail.com>
2022-01-31 02:01:34 +0100

    Normal

--- Utilities/Normal.cpp
+++ Utilities/Normal.cpp
@@ -1,5 +1,4 @@
 #include "Normal.hpp"
-
 #include <cmath>
 
 Normal::Normal(void)
@@ -11,17 +10,17 @@ Normal::Normal(double a)
 Normal::Normal(double x, double y, double z)
     : x(x), y(y), z(z) { }
 
-Normal::Normal(const Normal & n)
+Normal::Normal(const Normal& n)
     : x(n.x), y(n.y), z(n.z) { }
 
-Normal::Normal(const Vector3D & v)
+Normal::Normal(const Vector3D& v)
     : x(v.x), y(v.y), z(v.z) { }
 
 Normal::~Normal(void)
 {
 }
 
-Normal & Normal::operator=(const Normal & rhs) {
+Normal& Normal::operator=(const Normal& rhs) {
     if (this == &rhs)
         return *this;
 
@@ -32,7 +31,7 @@ Normal & Normal::operator=(const Normal & rhs) {
     return *this;
 }
 
-Normal & Normal::operator=(const Vector3D & rhs) {
+Normal& Normal::operator=(const Vector3D& rhs) {
     x = rhs.x;
     y = rhs.y;
     z = rhs.z;
@@ -40,52 +39,12 @@ Normal & Normal::operator=(const Vector3D & rhs) {
     return *this;
 }
 
-Normal & Normal::operator=(const Point3D & rhs) {
+Normal& Normal::operator=(const Point3D& rhs) {
     x = rhs.x;
     y = rhs.y;
     z = rhs.z;
 
     return *this;
-}
-
-Normal Normal::operator-() const {
-    return Normal(-x, -y, -z);
-}
-
-Normal Normal::operator+(const Normal & n) const {
-    return Normal(x + n.x, y + n.y, z + n.z);
-}
-
-Normal & Normal::operator+=(const Normal & n) {
-    x += n.x;
-    y += n.y;
-    z += n.z;
-
-    return *this;
-}
-
-double Normal::operator*(const Vector3D & v) const {
-    return x * v.x + y * v.y + z * v.z;
-}
-
-Normal Normal::operator*(const double a) const {
-    return Normal(x * a, y * a, z * a);
-}
-
-Normal operator*(const double a, const Normal & n) {
-    return Normal(a * n.x, a * n.y, a * n.z);
-}
-
-Vector3D operator+(const Vector3D & v, const Normal & n) {
-    return Vector3D(v.x + n.x, v.y + n.y, v.z + n.z);
-}
-
-Vector3D operator-(const Vector3D & v, const Normal & n) {
-    return Vector3D(v.x - n.x, v.y - n.y, v.z - n.z);
-}
-
-double operator*(const Vector3D & v, const Normal & n) {
-    return v.x * n.x + v.y * n.y + v.z * n.z;
 }
 
 void Normal::normalize() {
@@ -94,4 +53,3 @@ void Normal::normalize() {
     y /= length;
     z /= length;
 }
-
--- Utilities/Normal.hpp
+++ Utilities/Normal.hpp
@@ -9,18 +9,18 @@ public:
     Normal(void);
     Normal(double a);
     Normal(double x, double y, double z);
-    Normal(const Normal & n);
-    Normal(const Vector3D & n);
+    Normal(const Normal& n);
+    Normal(const Vector3D& n);
     virtual ~Normal(void);
 
-    Normal & operator=(const Normal & rhs);
-    Normal & operator=(const Vector3D & rhs);
-    Normal & operator=(const Point3D & rhs);
+    Normal& operator=(const Normal& rhs);
+    Normal& operator=(const Vector3D& rhs);
+    Normal& operator=(const Point3D& rhs);
 
     Normal operator-() const;
-    Normal operator+(const Normal & n) const;
-    Normal & operator+=(const Normal & n);
-    double operator*(const Vector3D & v) const;
+    Normal operator+(const Normal& n) const;
+    Normal& operator+=(const Normal& n);
+    double operator*(const Vector3D& v) const;
     Normal operator*(const double a) const;
 
     void normalize();
@@ -29,9 +29,54 @@ public:
     double x, y, z;
 };
 
-Normal operator*(const double a, const Normal & n);
-Vector3D operator+(const Vector3D & v, const Normal & n);
-Vector3D operator-(const Vector3D &, const Normal & n);
-double operator*(const Vector3D & v, const Normal & n);
+// lhs prototype
+Normal operator*(const double a, const Normal& n);
+Vector3D operator+(const Vector3D& v, const Normal& n);
+Vector3D operator-(const Vector3D& v, const Normal& n);
+double operator*(const Vector3D& v, const Normal& n);
+
+inline Normal Normal::operator-() const {
+    return Normal(-x, -y, -z);
+}
+
+inline Normal Normal::operator+(const Normal& n) const {
+    return Normal(x + n.x, y + n.y, z + n.z);
+}
+
+inline Normal& Normal::operator+=(const Normal& n) {
+    x += n.x;
+    y += n.y;
+    z += n.z;
+
+    return *this;
+}
+
+inline double Normal::operator*(const Vector3D& v) const {
+    return x * v.x + y * v.y + z * v.z;
+}
+
+inline Normal Normal::operator*(const double a) const {
+    return Normal(x * a, y * a, z * a);
+}
+
+// lhs
+inline Normal operator*(const double a, const Normal& n) {
+    return Normal(a * n.x, a * n.y, a * n.z);
+}
+
+// lhs
+inline Vector3D operator+(const Vector3D& v, const Normal& n) {
+    return Vector3D(v.x + n.x, v.y + n.y, v.z + n.z);
+}
+
+// lhs
+inline Vector3D operator-(const Vector3D& v, const Normal& n) {
+    return Vector3D(v.x - n.x, v.y - n.y, v.z - n.z);
+}
+
+// lhs
+inline double operator*(const Vector3D& v, const Normal& n) {
+    return v.x * n.x + v.y * n.y + v.z * n.z;
+}
 
 #endif // __NORMAL_HPP__
