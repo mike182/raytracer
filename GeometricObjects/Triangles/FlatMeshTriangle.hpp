bde507d28280f4bebbe6fd551cd024270a443b1f
mike <rodrig.tek@gmail.com>
2022-02-23 11:54:48 +0100

    GeometricObjects FlatMeshTriangle

---
+++ GeometricObjects/Triangles/FlatMeshTriangle.hpp
@@ -0,0 +1,22 @@
+#ifndef __FLATMESHTRIANGLE_HPP__
+#define __FLATMESHTRIANGLE_HPP__
+
+// FlatMeshTriangle is a MeshTriangle that uses flat shading with a constant normal
+// This uses the inherited shadow hit function from MeshTriangle
+
+#include "MeshTriangle.hpp"
+
+class FlatMeshTriangle: public MeshTriangle {
+public:
+    FlatMeshTriangle(void);
+    FlatMeshTriangle(Mesh* _meshPtr, const int i0, const int i1, const int i2);
+    virtual FlatMeshTriangle* clone(void) const;
+    FlatMeshTriangle(const FlatMeshTriangle& fmt);
+    virtual ~FlatMeshTriangle(void);
+
+    FlatMeshTriangle& operator=(const FlatMeshTriangle& rhs);
+
+    virtual bool hit(const Ray& ray, double& tmin, ShadeRec& sr) const;
+};
+
+#endif // __FLATMESHTRIANGLE_HPP__
